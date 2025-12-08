284aebb5b4a5f73ded08318228ba93e0613992cb
Aidan <aidanream01@gmail.com>
2025-12-08 07:43:56 -0800

    working plane

--- vulkanClothSim/main.cpp
+++ vulkanClothSim/main.cpp
@@ -1409,8 +1409,8 @@ private:
         float time = std::chrono::duration<float, std::chrono::seconds::period>(currentTime - startTime).count();
 
         UniformBufferObject ubo{};
-        //ubo.model = glm::rotate(glm::mat4(1.0f), time*glm::radians(90.0f), glm::vec3(0.0f, 0.0f, 1.0f));
-        ubo.model = glm::rotate(glm::mat4(1.0f), glm::radians(90.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+        //ubo.model = glm::rotate(glm::mat4(1.0f), time * glm::radians(90.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+        ubo.model = glm::rotate(glm::mat4(1.0f), time * glm::radians(90.0f), glm::vec3(1.0f, 0.0f, 0.0f));
 
 
         //ubo.model = glm::scale(ubo.model, glm::vec3(0.5, 0.5, 0.5)); 
